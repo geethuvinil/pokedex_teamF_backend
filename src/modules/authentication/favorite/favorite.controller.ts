1003362219acbf1568ead9723120eb923ebb06b9
geethu vininil <geethuajay@gmail.com>
2024-01-05 18:47:09 +0400

    add to favorites completed

--- src/modules/authentication/favorite/favorite.controller.ts
+++ src/modules/authentication/favorite/favorite.controller.ts
@@ -4,8 +4,13 @@ import { FavoriteService } from '../services/favorite/favorite.service';
 @Controller('favorite')
 export class FavoriteController {
     constructor(private favoriteService : FavoriteService){}
-    @Post()
-    async fav(@Body() favDetails){
+    @Post('add')
+    async favAdd(@Body() favDetails){
+        console.log('fav controller hitted',favDetails)
+    return this.favoriteService.addToFavorites(favDetails)
+    }
+    @Post('remove')
+    async favRemove(@Body() favDetails){
         console.log('fav controller hitted',favDetails)
     return this.favoriteService.addToFavorites(favDetails)
     }
--- src/modules/authentication/services/favorite/favorite.service.ts
+++ src/modules/authentication/services/favorite/favorite.service.ts
@@ -17,11 +17,13 @@ export class FavoriteService {
        console.log('favorite service1 entered')
         
         if(!!userObj){
+
             console.log('user found')
         console.log('datas are ',details['email'],details['pokeName'])
-       // userObj.favCharacters = [{characterName:details['pokeName']}]
- userObj.favCharacters= await userObj.updateOne({$push:{favCharacters: { characterName: details['pokeName'] } }})
-                userObj.save()
+ await userObj.favCharacters.push(details['pokeName'])
+
+
+userObj.save()
                 console.log('fafaffa',userObj)
                 response['statusCode'] = 201
                 response['message'] = 'Added to favorites'
@@ -35,27 +37,32 @@ export class FavoriteService {
         }
         
 
-        // async removeFromFavorites(email:string,characterName:string){
-
-        //     const userObj = await this.userModel.findOne({ email });
-        //    const response = {}
-        //   console.log('favorite service2 entered')
+        async removeFromFavorites(details:string){
+            console.log('datas are ',details['email'],details['pokeName'])
+            const userObj = await this.userModel.findOne({ email: details['email'] }).exec();
+            const response = {}
+            console.log('@@@@@',userObj)
+           console.log('favorite service1 entered')
+            
+            if(!!userObj){
+    
+                console.log('user found')
+            console.log('datas are ',details['email'],details['pokeName'])
+     await userObj.favCharacters.splice(details['pokeName'])
+    
+    
+    userObj.save()
+                    console.log('fafaffa',userObj)
+                    response['statusCode'] = 201
+                    response['message'] = 'Added to favorites'
            
-        //    if(!!userObj){
-               
-        //            userObj.pokeName = characterName
-        //            userObj.save()
-        //            response['statusCode'] = 205
-        //            response['message'] = 'Removed from favorites'
-          
-        //            return response
-        //        }
-        //        response['statusCode'] = 409
-        //        response['message'] = 'Something went wrong.'
-       
-        //        return response
-        //    }
-           
+                    return response
+                }
+                response['statusCode'] = 409
+                response['message'] = 'Something went wrong.'
+        
+                return response
+            }
 
     
 }
